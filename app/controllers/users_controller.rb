a8b6e09a762e00f33d106c5d35253a7c4cf6bd7a
Taiga Ueta <taiga.ueta@pwc.com>
2018-07-23 05:40:59 +0000

    新規登録機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,23 @@ class UsersController < ApplicationController
         @user = User.new
     end
     
+    def create
+      @user = User.new(user_params)
+      if @user.save
+      # 保存の成功した場合の処理
+        redirect_to tests_path
+      else
+        render 'new'
+      end
+    end
     
+    def show
+      @user = User.find(params[:id])
+    end
+
+    private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
