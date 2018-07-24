be9830fb45b7594854ca50395cd37b54303ac102
Toshiki Yoshimizu <toshiki.yoshimizu@pwc.com>
2018-07-24 16:30:45 +0000

    好み、記事表示機能

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -2,7 +2,8 @@ class FormsController < ApplicationController
   protect_from_forgery :except => [:destroy]
   before_action :check_login, only: [:new, :show, :edit, :update, :destroy]
   def index
-    
+    @preference = Preference.where(email: params[:email]).find_by(user_id: current_user.id)
+    @article = Article.where(arttype: params[:arttype]).last
   end
 
   def new
