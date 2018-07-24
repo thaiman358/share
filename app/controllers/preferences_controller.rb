df0c8a78011f0e2cf472dbcc8681b7daf758d585
Taiga Ueta <taiga.ueta@pwc.com>
2018-07-24 05:22:38 +0000

    preferenceテーブル作成

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,2 +1,14 @@
 class PreferencesController < ApplicationController
+    def new
+        @preference = Preference.new
+    end
+    
+    def create
+        @preference = Preference.new
+    end
+    
+    private
+    def preference_params
+      params.require(:preference).permit(:email, :preference)
+    end
 end
