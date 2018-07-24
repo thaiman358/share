59b8ae2d7ba7f887b4a54f72b07895d1cdbd0c4f
Toshiki Yoshimizu <toshiki.yoshimizu@pwc.com>
2018-07-24 06:33:03 +0000

    Articleのカラム名変更

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
   has_many :reads, dependent: :destroy
   has_many :read_users, through: :reads, source: :user
   validates :industry, presence: true
-  validates :type, presence: true
+  validates :arttype, presence: true
   validates :titile, presence: true, length: { in: 1..50 }
   validates :content, presence: true, length: { in: 1..500 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,7 @@
 
 # coding: utf-8
 Restaurant.create(name: 'PwC亭', category: '1', overview: '魚がおいしい', tel: '080-1234-5678',hour: '17:00~22:00', price: '15,000円', address: '日本')
+Restaurant.create(name: 'PwC亭', category: '2', overview: '肉がおいしい', tel: '080-1234-5679',hour: '17:00~23:00', price: '20,000円', address: '日本')
+Article.create(industry: '1', arttype: '2', title: 'AI', content: 'AIサイコー')
+User.create(name: 'a', email: 'a@a.a', password: 'aaaaaa', password_confirmation: 'aaaaaa', title: '2')
+User.create(name: 'b', email: 'b@b.b', password: 'bbbbbb', password_confirmation: 'bbbbbb', title: '2')
