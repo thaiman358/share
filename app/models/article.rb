1f61ea69a71ddb3e4880913dd7cfa74814322a47
Toshiki Yoshimizu <toshiki.yoshimizu@pwc.com>
2018-07-24 15:07:44 +0000

    restaurantのimageカラムを作成するmigrationファイルが2個あったので修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,6 @@ class Article < ApplicationRecord
   has_many :read_users, through: :reads, source: :user
   validates :industry, presence: true
   validates :arttype, presence: true
-  validates :titile, presence: true, length: { in: 1..50 }
+  validates :title, presence: true, length: { in: 1..50 }
   validates :content, presence: true, length: { in: 1..500 }
 end
---
+++ db/schema.rb
@@ -0,0 +1,88 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20180724051521) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.integer "industry"
+    t.integer "arttype"
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favs", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_favs_on_restaurant_id"
+    t.index ["user_id"], name: "index_favs_on_user_id"
+  end
+
+  create_table "preferences", force: :cascade do |t|
+    t.string "email"
+    t.string "preference"
+    t.string "unpreference"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_preferences_on_user_id"
+  end
+
+  create_table "reads", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_reads_on_article_id"
+    t.index ["user_id"], name: "index_reads_on_user_id"
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.integer "category"
+    t.text "overview"
+    t.string "hour"
+    t.string "tel"
+    t.string "price"
+    t.text "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "industry", default: 0
+    t.integer "title", default: 0
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  create_table "visits", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_visits_on_restaurant_id"
+    t.index ["user_id"], name: "index_visits_on_user_id"
+  end
+
+end
