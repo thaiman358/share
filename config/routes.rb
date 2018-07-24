1f09ffc2bc2ea10cc7bdfd729a7c837f5d615522
Toshiki Yoshimizu <toshiki.yoshimizu@pwc.com>
2018-07-24 15:11:22 +0000

    restaurantのimageカラムを作成するmigrationファイルが2個あったので修正

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,4 @@ Rails.application.routes.draw do
   resources:preferences
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
-  
-  
-  post '/forms/reserve', to:'forms#reserve'
 end
