23a269efa607fcb792b34f4c734c651f6505a486
Kenta Saichi <kenta.saichi@pwc.com>
2018-07-25 06:06:36 +0000

    　好み送信したらtopsに移動

--- db/schema.rb
+++ db/schema.rb
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20180725021443) do
   create_table "preferences", force: :cascade do |t|
     t.string "email"
     t.string "preference"
+    t.string "unpreference"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -58,8 +59,8 @@ ActiveRecord::Schema.define(version: 20180725021443) do
     t.integer "category"
     t.text "overview"
     t.string "hour"
-    t.string "tel"
-    t.string "price"
+    t.integer "tel"
+    t.integer "price"
     t.text "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
