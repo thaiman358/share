160afe96f2bd289463e562748c3f7f448124a968
Toshiki Yoshimizu <toshiki.yoshimizu@pwc.com>
2018-07-25 04:09:43 +0000

    記事選択機能

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -2,9 +2,14 @@ class FormsController < ApplicationController
   protect_from_forgery :except => [:destroy]
   before_action :check_login, only: [:new, :show, :edit, :update, :destroy]
   def index
+    arttype = params[:arttype]
     @restaurant = Restaurant.find_by(id: params[:restaurant_id])
     @preference = Preference.where(email: params[:email]).find_by(user_id: current_user.id)
-    @article = Article.where(arttype: params[:arttype]).last
+    if arttype == '2' then #社外＋初回は料理に関する記事を表示
+      @article = Article.where(arttype: arttype, restaurant_id: @restaurant.id).last
+    else　#社内、社外の初回以外は事例・業界に関する記事を表示（1:事例、3:業界）
+      @article = Article.where(arttype: arttype, industry: current_user.industry).last
+    end
     Visit.create(user_id: current_user.id, restaurant_id: @restaurant.id)
     Read.create(user_id: current_user.id, article_id: @article.id)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180724051521) do
+ActiveRecord::Schema.define(version: 20180725021443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20180724051521) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "restaurant_id"
+    t.index ["restaurant_id"], name: "index_articles_on_restaurant_id"
   end
 
   create_table "favs", force: :cascade do |t|
@@ -85,4 +87,5 @@ ActiveRecord::Schema.define(version: 20180724051521) do
     t.index ["user_id"], name: "index_visits_on_user_id"
   end
 
+  add_foreign_key "articles", "restaurants"
 end
