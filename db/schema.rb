c033c9d81682dbaf80a53ea7510aac0be9e53e18
Taiga Ueta <taiga.ueta@pwc.com>
2018-07-25 01:46:12 +0000

    prefer ask画面完成

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20180724051521) do
 
   create_table "articles", force: :cascade do |t|
     t.integer "industry"
-    t.integer "arttype"
+    t.integer "type"
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
