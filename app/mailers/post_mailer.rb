e95ccd7f67e63fc2c9f90a2222cee5ab6f21e433
Kenta Saichi <kenta.saichi@pwc.com>
2018-07-25 04:12:07 +0000

    mailersの編集

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -1,2 +1,9 @@
 class PostMailer < ApplicationMailer
+  default from: "tashackathon2018@gmail.com"
+
+  def post_email(user, post)
+    @clientname = preference.email
+    @id =preference.id
+    mail to: user.email, subject: "PreferenceAnswerForm"
+  end
 end
