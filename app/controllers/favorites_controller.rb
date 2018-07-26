2f1a9c08116bfb8a4b1269abc4bdff8be43cbdfd
Taiga Ueta <taiga.ueta@pwc.com>
2018-07-26 05:49:03 +0000

    favs-debug

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,10 +6,16 @@ class FavoritesController < ApplicationController
   end
   
   def create
-    fav = current_user.favs.create(restaurant_id: params[:restaurant_id])
+    @fav = current_user.favs.create(restaurant_id: params[:restaurant_id])
+    if @fav.save
+      redirect_to restaurant_path(params[:restaurant_id])      
+    end
   end
 
   def destroy
-    fav = current_user.favs.find_by(id: params[:id]).destroy
+    @fav = current_user.favs.find_by(id: params[:id]).destroy
+    if @fav.save
+      redirect_to restaurant_path(params[:restaurant_id])
+    end
   end
 end
