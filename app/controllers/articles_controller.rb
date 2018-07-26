1d60a28cae0da7588e50d297f4c5692db2e1f5bf
Taiga Ueta <taiga.ueta@pwc.com>
2018-07-26 00:17:40 +0000

    未Login状態でRestaurantのNewアクションを実行できない

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,9 @@
 class ArticlesController < ApplicationController
+   # before_action :login_check
+    
+#    def login_check
+#      unless current_user
+#        redirect_to new_session_path
+#      end
+#    end
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,7 @@
 class RestaurantsController < ApplicationController
   # before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+  before_action :login_check, only: [:new]
+  
   def new
     @restaurant = Restaurant.new
   end
@@ -48,6 +50,13 @@ class RestaurantsController < ApplicationController
     def restaurant_params
       params.require(:restaurant).permit(:name, :category, :overview, :hour, :tel, :price,:address, :image)
     end
+    
+    def login_check
+      unless current_user
+        redirect_to new_session_path
+      end
+    end
+
     # def set_restaurant
     # 　@restaurant = Restaurant.find(params[:id])
     # end
